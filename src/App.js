126df504b71f156da52d525ba460342737cb2b8c
pranavv481 <pranavv481@gmail.com>
2020-05-25 13:59:39 +0530

    add counter

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ClassCounter />
+        <HookCounter />
+      </div>
+    );
+  }
+}
+
+export default App;
